659096c4747b61fe26c84b8ff700eeddecfd4007
Arshad <ahmed.a0611@gmail.com>
2023-07-30 19:12:30 +0530

    practice

--- JavaScript Practice/Javascript-fundamental.js
+++ JavaScript Practice/Javascript-fundamental.js
@@ -11,3 +11,23 @@ function myFunction(a, b) {
     return a === b;
   }
 
+
+  // 3. Get type of value - Write a function that takes a value as argument. Return the type of the value.
+
+  function myFunction(a) {
+    return typeof a;
+ }
+
+ // 4. Get nth character of string - Write a function that takes a string (a) and a number (n) as argument. Return the nth character of 'a'.
+
+ function myFunction(a, n) {
+    return a[n - 1];
+ }
+
+ // 5. Remove first n characters of string - Write a function that takes a string (a) as argument. Remove the first 3 characters of a. Return the result
+
+ function myFunction(a) {
+    return a.slice(3);
+ }
+
+ // 6
