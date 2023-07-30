70a3cfdb457f39c61060fe40c366bda4a27dad0e
Arshad <ahmed.a0611@gmail.com>
2023-07-30 19:43:44 +0530

    math operation

--- JavaScript Practice/Javascript-fundamental.js
+++ JavaScript Practice/Javascript-fundamental.js
@@ -30,8 +30,46 @@ function myFunction(a, b) {
 //     return a.slice(3);
 //  }
 
- // 6 Get last n characters of string - Write a function that takes a string as argument. Extract the last 3 characters from the string. Return the result
+ // 6. Get last n characters of string - Write a function that takes a string as argument. Extract the last 3 characters from the string. Return the result
 
  function myFunction(str){
     return str.slice(-3) 
  }
+
+ // 7. Get first n characters of string - Write a function that takes a string (a) as argument. Get the first 3 characters of a. Return the result
+
+
+ function myFunction(a) {
+    return a.slice(0, 3);
+ }
+
+ // 8. Find the position of one string in another - Write a function that takes a string as argument. The string contains the substring 'is'. Return the index of 'is'.
+
+ function myFunction(a) {
+    return a.indexOf('is');
+ }
+
+ // 9. Extract first half of string - Write a function that takes a string (a) as argument. Extract the first half a. Return the result
+
+ function myFunction(a) {
+    return a.slice(0, a.length / 2);
+ }
+
+ // 10. Remove last n characters of string - Write a function that takes a string (a) as argument. Remove the last 3 characters of a. Return the result
+
+ function myFunction(a){
+    return a.slice(0, a.lenght -3)
+}
+// OR
+
+function myFunction(a){
+    return a.slice(0, -3)
+}
+
+// 11. Return the percentage of a number - Write a function that takes two numbers (a and b) as argument. Return b percent of a
+
+function
+myFunction(a, b)
+{
+return b % a;
+}
