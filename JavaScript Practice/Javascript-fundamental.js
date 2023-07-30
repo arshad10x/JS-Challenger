b30e2e86fccf43c303247b1f0e07f005766cc42f
Arshad <ahmed.a0611@gmail.com>
2023-07-30 18:59:42 +0530

    fundamentals

--- JavaScript Practice/Javascript-fundamental.js
+++ JavaScript Practice/Javascript-fundamental.js
@@ -1,6 +1,13 @@
-// 1. Write a function that takes two numbers (a and b) as argument. Sum a and b. Return the result
+// 1. SUM OF TWO NUMBER - Write a function that takes two numbers (a and b) as argument. Sum a and b. Return the result
 
 function myFunction (a,b){
     return a + b;
 }
 myFunction(2,3);
+
+// 2. COMPARISION OPERATOR, STRICT EQUALITY - Write a function that takes two values, say a and b, as arguments. Return true if the two values are equal and of the same type
+
+function myFunction(a, b) {
+    return a === b;
+  }
+
