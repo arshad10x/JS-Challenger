5fa0bea5315db092406dbb5601c97edfa9413623
Arshad <ahmed.a0611@gmail.com>
2023-07-30 19:52:48 +0530

    math operation

--- JavaScript Practice/Javascript-fundamental.js
+++ JavaScript Practice/Javascript-fundamental.js
@@ -71,5 +71,13 @@ function myFunction(a){
 function
 myFunction(a, b)
 {
-return b % a;
+return b /100 *a;
 }
+
+// 12. Basic JavaScript math operators - Write a function that takes 6 values (a,b,c,d,e,f) as arguments. Sum a and b. Then substract by c. Then multiply by d and divide by e. Finally raise to the power of f and return the result. Tipp: mind the order.
+
+function myFunction(a, b, c, d, e, f) {
+    return (((a + b - c) * d) / e) ** f;
+ }
+
+//  13. Check whether a string contains another string and concatenate - Write a function that takes two strings (a and b) as arguments. If a contains b, append b to the beginning of a. If not, append it to the end. Return the concatenation
